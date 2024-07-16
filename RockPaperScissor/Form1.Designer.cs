a8c53cc175ab3fd88fbffb568dde84edf828cc15
nail rzayev <rzayevnail2003@gmail.com>
2024-07-16 11:49:15 +0400

    first commit

---
+++ RockPaperScissor/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace RockPaperScissor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PicturePlayer1 = new System.Windows.Forms.PictureBox();
+            this.PictureBoxPlayer2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PicturePlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxPlayer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PicturePlayer1
+            // 
+            this.PicturePlayer1.Image = ((System.Drawing.Image)(resources.GetObject("PicturePlayer1.Image")));
+            this.PicturePlayer1.Location = new System.Drawing.Point(191, 137);
+            this.PicturePlayer1.Name = "PicturePlayer1";
+            this.PicturePlayer1.Size = new System.Drawing.Size(200, 196);
+            this.PicturePlayer1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PicturePlayer1.TabIndex = 0;
+            this.PicturePlayer1.TabStop = false;
+            // 
+            // PictureBoxPlayer2
+            // 
+            this.PictureBoxPlayer2.Image = ((System.Drawing.Image)(resources.GetObject("PictureBoxPlayer2.Image")));
+            this.PictureBoxPlayer2.Location = new System.Drawing.Point(665, 137);
+            this.PictureBoxPlayer2.Name = "PictureBoxPlayer2";
+            this.PictureBoxPlayer2.Size = new System.Drawing.Size(200, 196);
+            this.PictureBoxPlayer2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBoxPlayer2.TabIndex = 1;
+            this.PictureBoxPlayer2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("French Script MT", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(41, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Rock";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("French Script MT", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(41, 187);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Paper";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("French Script MT", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(41, 244);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 31);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Scissor";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Script MT Bold", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(191, 336);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 49);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Player1";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Script MT Bold", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(665, 337);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 49);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Player2";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Script MT Bold", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(464, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 54);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Score :";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Script MT Bold", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label5.Location = new System.Drawing.Point(397, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 123);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Script MT Bold", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label4.Location = new System.Drawing.Point(554, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 123);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Script MT Bold", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label6.Location = new System.Drawing.Point(494, 172);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 147);
+            this.label6.TabIndex = 11;
+            this.label6.Text = ":";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(58, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(198, 109);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 14;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Script MT Bold", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Teal;
+            this.label9.Location = new System.Drawing.Point(262, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(666, 89);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Rock Paper Scissor ";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(851, 473);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 29);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Restart";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Script MT Bold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label10.Location = new System.Drawing.Point(542, 337);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(55, 66);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "5";
+            // 
+            // label11
+            // 
+            this.label11.Font = new System.Drawing.Font("Script MT Bold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label11.Location = new System.Drawing.Point(446, 336);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(102, 66);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Time :";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.ForeColor = System.Drawing.Color.Black;
+            this.button5.Location = new System.Drawing.Point(851, 426);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(89, 29);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "Start";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(968, 536);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PictureBoxPlayer2);
+            this.Controls.Add(this.PicturePlayer1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PicturePlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxPlayer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PicturePlayer1;
+        private System.Windows.Forms.PictureBox PictureBoxPlayer2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ RockPaperScissor/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RockPaperScissor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string bizimSecdiyimiz = "";
+        string[] images = { "C:\\Users\\nailr\\Downloads\\rock.jpg", "C:\\Users\\nailr\\Downloads\\paper.jpg", "C:\\Users\\nailr\\Downloads\\scissors.jpg" };
+        string defaultImage = "C:\\Users\\nailr\\Downloads\\qq.jpg";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PicturePlayer1.Image = System.Drawing.Image.FromFile(images[0]);
+            bizimSecdiyimiz = images[0];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PicturePlayer1.Image = System.Drawing.Image.FromFile(images[1]);
+            bizimSecdiyimiz = images[1];
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PicturePlayer1.Image = System.Drawing.Image.FromFile(images[2]);
+            bizimSecdiyimiz = images[2];
+        }
+        public int timer = 6;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer--;
+            if (timer == 0)
+            {
+                timer3.Start();
+                Random random = new Random();
+                int randomIndex1 = random.Next(images.Length);
+                string randomImage = images[randomIndex1];
+                PictureBoxPlayer2.Image = System.Drawing.Image.FromFile(randomImage);
+                if ((bizimSecdiyimiz == images[0] && randomImage == images[2]) || (bizimSecdiyimiz == images[2] && randomImage == images[0]))
+                {
+                    if (bizimSecdiyimiz == images[0])
+                    {
+                        player1++;
+                    }
+                    if (randomImage == images[0])
+                    {
+                        player2++;
+                    }
+                }
+                if ((bizimSecdiyimiz == images[0] && randomImage == images[1]) || (bizimSecdiyimiz == images[1] && randomImage == images[0]))
+                {
+                    if (bizimSecdiyimiz == images[1])
+                    {
+                        player1++;
+                    }
+                    if (randomImage == images[1])
+                    {
+                        player2++;
+                    }
+                }
+                if ((bizimSecdiyimiz == images[1] && randomImage == images[2]) || (bizimSecdiyimiz == images[2] && randomImage == images[1]))
+                {
+                    if (bizimSecdiyimiz == images[2])
+                    {
+                        player1++;
+                    }
+                    if (randomImage == images[2])
+                    {
+                        player2++;
+                    }
+                }
+            }
+            if (timer == 0)
+            {
+                timer1.Stop();
+            }
+            label5.Text = player1.ToString();
+            label4.Text = player2.ToString();
+            label10.Text = timer.ToString();//time
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            if (timer == 0)
+            {
+                PicturePlayer1.Image = System.Drawing.Image.FromFile(defaultImage);
+                PictureBoxPlayer2.Image = System.Drawing.Image.FromFile(defaultImage);
+                timer = 6;
+            }
+        }
+        public int player1 = 0;
+        public int player2 = 0;
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label4.Text = "0";
+            label5.Text = "0";
+        }
+    }
+}
